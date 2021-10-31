3539fc532f6c2f2bc36b1e1e0f7a4c184fcc4289
driller <eleshis@gmail.com>
2021-10-31 20:22:43 +0900

    add docstring sample in clinent.py

--- pybotters/client.py
+++ pybotters/client.py
@@ -19,6 +19,40 @@ logger = logging.getLogger(__name__)
 
 
 class Client:
+    """
+    HTTPリクエストクライアントクラス
+
+    .. note::
+        引数 apis は省略できます。
+
+    :Example:
+
+    .. code-block:: python
+
+        async def main():
+            async with pybotters.Client(apis={'example': ['KEY', 'SECRET']}) as client:
+                r = await client.get('https://...', params={'foo': 'bar'})
+                print(await r.json())
+    
+    .. code-block:: python
+
+        async def main():
+            async with pybotters.Client(apis={'example': ['KEY', 'SECRET']}) as client:
+                wstask = await client.ws_connect('wss://...', send_json={'foo': 'bar'}, hdlr_json=pybotters.print_handler)
+                await wstask
+                # Ctrl+C to break
+
+    .. code-block:: python
+
+        r = pybotters.get('https://...', params={'foo': 'bar'}, apis={'example': ['KEY', 'SECRET']})
+        print(r.text())
+        print(r.json())
+    
+    .. code-block:: python
+    
+        pybotters.ws_connect('wss://...', send_json={'foo': 'bar'}, hdlr_json=pybotters.print_handler, apis={'example': ['KEY', 'SECRET']})
+        # Ctrl+C to break
+    """
     _session: aiohttp.ClientSession
     _base_url: str
 
@@ -28,6 +62,10 @@ class Client:
         base_url: str = '',
         **kwargs: Any,
     ) -> None:
+        """
+        :param apis: APIキー・シークレットのデータ(optional) ex: {'exchange': ['key', 'secret']}
+        :param base_url: リクエストメソッドの url の前方に自動付加するURL(optional)
+        """
         self._session = aiohttp.ClientSession(
             request_class=ClientRequest,
             ws_response_class=ClientWebSocketResponse,
@@ -78,6 +116,14 @@ class Client:
         data: Any = None,
         **kwargs: Any,
     ) -> _RequestContextManager:
+        """
+        :param method: GET, POST, PUT, DELETE などのHTTPメソッド
+        :param url: リクエストURL
+        :param params: URLのクエリ文字列(optional)
+        :param data: リクエストボディ(optional)
+        :param headers: リクエストヘッダー(optional)
+        :param auth: API自動認証の機能の有効/無効。デフォルトで有効。auth=Noneを指定することで無効になります(optional)
+        """
         return self._request(method, url, params=params, data=data, **kwargs)
 
     def get(
@@ -126,6 +172,15 @@ class Client:
         hdlr_json: Optional[WsJsonHandler] = None,
         **kwargs: Any,
     ) -> asyncio.Task:
+        """
+        :param url: WebSocket URL
+        :param send_str: WebSocketで送信する文字列。文字列、または文字列のリスト形式(optional)
+        :param send_json: WebSocketで送信する辞書オブジェクト。辞書、または辞書のリスト形式(optional)
+        :param hdlr_str: WebSocketの受信データをハンドリングする関数。第1引数 msg に _str_型, 第2引数 ws に WebSocketClientResponse 型の変数が渡されます(optional)
+        :param hdlr_json: WebSocketの受信データをハンドリングする関数。第1引数 msg に Any 型(JSON-like), 第2引数 ws に WebSocketClientResponse 型の変数が渡されます(optional)
+        :param headers: リクエストヘッダー(optional)
+        :param auth: API自動認証の機能の有効/無効。デフォルトで有効。auth=Noneを指定することで無効になります(optional)
+        """
         event = asyncio.Event()
         task = asyncio.create_task(
             ws_run_forever(
