0c6176268673711aa9e796e2f1a8439278d5c56c
MtkN1 <51289448+MtkN1@users.noreply.github.com>
2023-09-16 21:30:54 +0900

    Extend extensions

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,17 @@ author = "MtkN1XBt"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.autosummary"]
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.linkcode",
+    "sphinx.ext.napoleon",
+    "sphinx_copybutton",
+    "sphinx_inline_tabs",
+    "sphinxcontrib.spelling",
+    "sphinxcontrib_trio",
+    "sphinxext.opengraph",
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
