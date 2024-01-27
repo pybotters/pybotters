6009e9cb704d769f26ed402beb5105f39ed166f4
MtkN1 <51289448+MtkN1@users.noreply.github.com>
2024-01-27 23:50:10 +0900

    Add order book store examples

---
+++ example/datastore/bitflyer.py
@@ -0,0 +1,39 @@
+import asyncio
+
+import pybotters
+from rich import print
+
+
+async def main():
+    async with pybotters.Client() as client:
+        store = pybotters.bitFlyerDataStore()
+
+        await client.ws_connect(
+            "wss://ws.lightstream.bitflyer.com/json-rpc",
+            send_json=[
+                {
+                    "method": "subscribe",
+                    "params": {"channel": "lightning_board_snapshot_FX_BTC_JPY"},
+                    "id": 1,
+                },
+                {
+                    "method": "subscribe",
+                    "params": {"channel": "lightning_board_FX_BTC_JPY"},
+                    "id": 2,
+                },
+            ],
+            hdlr_json=store.onmessage,
+        )
+
+        while True:
+            orderbook = store.board.sorted(limit=2)
+            print(orderbook)
+
+            await store.board.wait()
+
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        pass
---
+++ example/datastore/bitget.py
@@ -0,0 +1,31 @@
+import asyncio
+
+import pybotters
+from rich import print
+
+
+async def main():
+    async with pybotters.Client() as client:
+        store = pybotters.BitgetDataStore()
+
+        await client.ws_connect(
+            "wss://ws.bitget.com/mix/v1/stream",
+            send_json={
+                "op": "subscribe",
+                "args": [{"instType": "mc", "channel": "books", "instId": "BTCUSDT"}],
+            },
+            hdlr_json=store.onmessage,
+        )
+
+        while True:
+            orderbook = store.orderbook.sorted(limit=2)
+            print(orderbook)
+
+            await store.orderbook.wait()
+
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        pass
