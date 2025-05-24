3930e46920163006f9c11193039d10418f2512d8
MtkN1 <51289448+MtkN1@users.noreply.github.com>
2025-05-24 22:19:39 +0900

    Update gmocoin.py

--- pybotters/models/gmocoin.py
+++ pybotters/models/gmocoin.py
@@ -141,10 +141,10 @@ class GMOCoinDataStore(DataStoreCollection):
 
         対応エンドポイント
 
-        - GET /private/v1/latestExecutions (:attr:`.CoincheckDataStore.executions`)
-        - GET /private/v1/activeOrders (:attr:`.CoincheckDataStore.orders`)
-        - GET /private/v1/openPositions (:attr:`.CoincheckDataStore.positions`)
-        - GET /private/v1/positionSummary (:attr:`.CoincheckDataStore.position_summary`)
+        - GET /private/v1/latestExecutions (:attr:`.GMOCoinDataStore.executions`)
+        - GET /private/v1/activeOrders (:attr:`.GMOCoinDataStore.orders`)
+        - GET /private/v1/openPositions (:attr:`.GMOCoinDataStore.positions`)
+        - GET /private/v1/positionSummary (:attr:`.GMOCoinDataStore.position_summary`)
         """
         for f in asyncio.as_completed(aws):
             resp = await f
